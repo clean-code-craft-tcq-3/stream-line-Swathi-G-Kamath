255860b63e5f23a89eb987f3703a310e89d99880
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-07-06 11:04:07 +0530

    Update testBatterySensor.cpp

--- testBatterySensor.cpp
+++ testBatterySensor.cpp
@@ -6,13 +6,13 @@ TEST_CASE("Test the generation of the sensor values")
 {
     batterySensor batterySensorValues=getBatteryParameters();
 
-    REQUIRE(batterySensorValues.SOCList.size()==60);
+    REQUIRE(batterySensorValues.SOCList.size()==100);
     for(int i=0;i<60;i++)
     {
         REQUIRE(20<=batterySensorValues.SOCList[i]);
         REQUIRE(batterySensorValues.SOCList[i]<=80);
     }
-    REQUIRE(batterySensorValues.tempValueList.size()==60);
+    REQUIRE(batterySensorValues.tempValueList.size()==100);
     for(int i=0;i<60;i++)
     {
         REQUIRE(-20<=batterySensorValues.tempValueList[i]);
@@ -25,8 +25,8 @@ TEST_CASE("Test if the battery data is displaying on the console")
   batterySensor batterySensorValues=getBatteryParameters();
   mockSensorData mockSensorDataInstance;
   displayBatterSensorValues(batterySensorValues,mockSensorDataInstance);
-  REQUIRE(mockSensorDataInstance.displayCounter==60);
-  REQUIRE(mockSensorDataInstance.formatCounter==60);
+  REQUIRE(mockSensorDataInstance.displayCounter==100);
+  REQUIRE(mockSensorDataInstance.formatCounter==100);
 }
 
 
@@ -42,6 +42,6 @@ TEST_CASE("Test where data is sent to receiver through mock data")
     mockSensorData mockSensorDataInstance;
     bool output=true;
     REQUIRE(sendBatteryParameters(mockSensorDataInstance)==output);
-    REQUIRE(mockSensorDataInstance.displayCounter==60);
-    REQUIRE(mockSensorDataInstance.formatCounter==60);
+    REQUIRE(mockSensorDataInstance.displayCounter==100);
+    REQUIRE(mockSensorDataInstance.formatCounter==100);
 }
