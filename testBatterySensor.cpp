bb7e2fde9e5d5b3d1d05beb507e3948e14796079
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-07-06 10:21:04 +0530

    Update testBatterySensor.cpp

--- testBatterySensor.cpp
+++ testBatterySensor.cpp
@@ -1,3 +1,7 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "typewise-alert.h"
 #include "BatterySensorDataSender.hpp"
 
 TEST_CASE("Test the generation of the sensor values")
