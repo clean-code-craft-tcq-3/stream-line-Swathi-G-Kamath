b19fe689c988ecd92c74a09739ac526d21d90afd
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-07-22 11:28:24 +0530

    Update BatterySensorDataReceiver.h

--- Receiver/BatterySensorDataReceiver.h
+++ Receiver/BatterySensorDataReceiver.h
@@ -1,1 +1,29 @@
+#ifndef RECEIVER_H
+#define RECEIVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define SENSOR_DATA_SIZE 50 
+
+typedef struct 
+{
+    float minValue;
+    float maxValue;
+    float average;
+    float movingAverage;
+}statsData;
+
+
+void displayStatistics(statsData result);
+void getDataFromSender();
+float getMinValueFromSensorDataStream(float SensorData[], int streamDataSize);
+float getMaxValueFromSensorDataStream(float SensorData[], int streamDataSize);
+float calculateAverageValueFromSensorDataStream(float SensorData[], int streamDataSize);
+float calculateSimpleMovingAverage(float SensorData[], int entrySize, int lastSampleValue);
+statsData computeStatistics(float sensorData[], int streamDataSize, int movingAverageRange);
+
+
+#endif
 
